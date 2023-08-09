40966b05f6400866c6fa59ccd202b6b62e432c3e
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-09 19:30:51 -0300

    iniciado o repositório local

---
+++ index.js
@@ -0,0 +1,18 @@
+const carros = []
+
+const sistemaCRUD = prompt(`Bem-vindo ao sistema de CRUD de veículos:\nNo momento, o sistema tem X carros cadastrados\nEscolha uma das opções para interagir com o sistema:\n1 - Cadastrar veículo;\n2 - Listar todos os veículos;\n3 - Filtrar veículos por marca;\n4 - Atualizar veículo;\n5 - Remover veículo;\n6 - Sair do sistema.`)
+
+  switch(sistemaCRUD){
+    case '1':
+        
+      break
+  }
+
+
+// 1 - Cadastrar veículo
+// -> Ao entrar nesta opção o sistema deve pedir os seguintes
+// dados: modelo, marca, ano, cor e preco.
+// -> O veículo deve ser adicionado na lista de veículos que
+// armazena todos os veículos cadastrados
+// -> Todo veículo deve ter um identificador único. Este
+// identificador deve ser gerado de forma automática
